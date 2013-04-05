488fe5944fa00dbb703924ecb7c1554915967d8a
Adam Cornille <diggity@adamcornille.com>
2013-04-05 14:38:51 -0700

    Revised to be homomorphic to humanhash.py
    Modified algorithm so that outputs match humanhash.py given any input.
    Editted default wordlist accordingly.
    Also added the "compress" and "uuid" methods found in humanhash.py .
    To more closely match humanhash.py, the module now defines a constructor
    "HumanHasher" which must be instantiated. ie: HumanHash = new
    HumanHasher();

--- humanhash.js
+++ humanhash.js
@@ -1,103 +1,51 @@
 ﻿;(function(global){
-    // Word lists taken from the PGP word list located at
-    // http://en.wikipedia.org/wiki/PGP_word_list
-    var evenWords = [
-        'aardvark', 'absurd', 'accrue', 'acme', 'adrift', 'adult',
-        'afflict', 'ahead', 'aimless', 'Algol', 'allow', 'alone',
-        'ammo', 'ancient', 'apple', 'artist', 'assume', 'Athens',
-        'atlas', 'Aztec', 'baboon', 'backfield', 'backward', 'banjo',
-        'beaming', 'bedlamp', 'beehive', 'beeswax', 'befriend', 'Belfast',
-        'berserk', 'billiard', 'bison', 'blackjack', 'blockade', 'blowtorch',
-        'bluebird', 'bombast', 'bookshelf', 'brackish', 'breadline', 'breakup',
-        'brickyard', 'briefcase', 'Burbank', 'button', 'buzzard', 'cement',
-        'chairlift', 'chatter', 'checkup', 'chisel', 'choking', 'chopper',
-        'Christmas', 'clamshell', 'classic', 'classroom', 'cleanup', 'clockwork',
-        'cobra', 'commence', 'concert', 'cowbell', 'crackdown',
-        'cranky', 'crowfoot', 'crucial', 'crumpled', 'crusade', 'cubic',
-        'dashboard', 'deadbolt', 'deckhand', 'dogsled', 'dragnet', 'drainage',
-        'dreadful', 'drifter', 'dropper', 'drumbeat', 'drunken', 'Dupont',
-        'dwelling', 'eating', 'edict', 'egghead', 'eightball', 'endorse',
-        'endow', 'enlist', 'erase', 'escape', 'exceed', 'eyeglass',
-        'eyetooth', 'facial', 'fallout', 'flagpole', 'flatfoot', 'flytrap',
-        'fracture', 'framework', 'freedom', 'frighten', 'gazelle', 'Geiger',
-        'glitter', 'glucose', 'goggles', 'goldfish', 'gremlin', 'guidance',
-        'hamlet', 'highchair', 'hockey', 'indoors', 'indulge', 'inverse',
-        'involve', 'island', 'jawbone', 'keyboard', 'kickoff', 'kiwi',
-        'klaxon', 'locale', 'lockup', 'merit', 'minnow',
-        'miser', 'Mohawk', 'mural', 'music', 'necklace', 'Neptune',
-        'newborn', 'nightbird', 'Oakland', 'obtuse', 'offload', 'optic',
-        'orca', 'payday', 'peachy', 'pheasant', 'physique', 'playhouse',
-        'Pluto', 'preclude', 'prefer', 'preshrunk', 'printer', 'prowler',
-        'pupil', 'puppy', 'python', 'quadrant', 'quiver', 'quota',
-        'ragtime', 'ratchet', 'rebirth', 'reform', 'regain', 'reindeer',
-        'rematch', 'repay', 'retouch', 'revenge', 'reward', 'rhythm',
-        'ribcage', 'ringbolt', 'robust', 'rocker', 'ruffled', 'sailboat',
-        'sawdust', 'scallion', 'scenic', 'scorecard', 'Scotland', 'seabird',
-        'select', 'sentence', 'shadow', 'shamrock', 'showgirl', 'skullcap',
-        'skydive', 'slingshot', 'slowdown', 'snapline', 'snapshot',
-        'snowcap', 'snowslide', 'solo', 'southward', 'soybean', 'spaniel',
-        'spearhead', 'spellbind', 'spheroid', 'spigot', 'spindle', 'spyglass',
-        'stagehand', 'stagnate', 'stairway', 'standard', 'stapler', 'steamship',
-        'sterling', 'stockman', 'stopwatch', 'stormy', 'sugar', 'surmount',
-        'suspense', 'sweatband', 'swelter', 'tactics', 'talon', 'tapeworm',
-        'tempest', 'tiger', 'tissue', 'tonic', 'topmost', 'tracker',
-        'transit', 'trauma', 'treadmill', 'Trojan', 'trouble', 'tumor',
-        'tunnel', 'tycoon', 'uncut', 'unearth', 'unwind', 'uproot',
-        'upset', 'upshot', 'vapor', 'village', 'virus', 'Vulcan',
-        'waffle', 'wallet', 'watchword', 'wayside', 'willow', 'woodlark',
-        'Zulu'
-    ];
-    var oddWords = [
-        'adroitness', 'adviser', 'aftermath', 'aggregate', 'alkali', 'almighty',
-        'amulet', 'amusement', 'antenna', 'applicant', 'Apollo', 'armistice',
-        'article', 'asteroid', 'Atlantic', 'atmosphere', 'autopsy', 'Babylon',
-        'backwater', 'barbecue', 'belowground', 'bifocals', 'bodyguard', 'bookseller',
-        'borderline', 'bottomless', 'Bradbury', 'bravado', 'Brazilian', 'breakaway',
-        'Burlington', 'businessman', 'butterfat', 'Camelot', 'candidate', 'cannonball',
-        'Capricorn', 'caravan', 'caretaker', 'celebrate', 'cellulose', 'certify',
-        'chambermaid', 'Cherokee', 'Chicago', 'clergyman', 'coherence', 'combustion',
-        'commando', 'company', 'component', 'concurrent', 'confidence', 'conformist',
-        'congregate', 'consensus', 'consulting', 'corporate', 'corrosion', 'councilman',
-        'crossover', 'crucifix', 'cumbersome', 'customer', 'Dakota',
-        'decadence', 'December', 'decimal', 'designing', 'detector', 'detergent',
-        'determine', 'dictator', 'dinosaur', 'direction', 'disable', 'disbelief',
-        'disruptive', 'distortion', 'document', 'embezzle', 'enchanting', 'enrollment',
-        'enterprise', 'equation', 'equipment', 'escapade', 'Eskimo', 'everyday',
-        'examine', 'existence', 'exodus', 'fascinate', 'filament', 'finicky',
-        'forever', 'fortitude', 'frequency', 'gadgetry', 'Galveston', 'getaway',
-        'glossary', 'gossamer', 'graduate', 'gravity', 'guitarist', 'hamburger',
-        'Hamilton', 'handiwork', 'hazardous', 'headwaters', 'hemisphere', 'hesitate',
-        'hideaway', 'holiness', 'hurricane', 'hydraulic', 'impartial', 'impetus',
-        'inception', 'indigo', 'inertia', 'infancy', 'inferno', 'informant',
-        'insincere', 'insurgent', 'integrate', 'intention', 'inventive',
-        'Istanbul', 'Jamaica', 'Jupiter', 'leprosy', 'letterhead', 'liberty',
-        'maritime', 'matchmaker', 'maverick', 'Medusa', 'megaton', 'microscope',
-        'microwave', 'midsummer', 'millionaire', 'miracle', 'misnomer', 'molasses',
-        'molecule', 'Montana', 'monument', 'mosquito', 'narrative', 'nebula',
-        'newsletter', 'Norwegian', 'October', 'Ohio', 'onlooker', 'opulent',
-        'Orlando', 'outfielder', 'Pacific', 'pandemic', 'Pandora', 'paperweight',
-        'paragon', 'paragraph', 'paramount', 'passenger', 'pedigree', 'Pegasus',
-        'penetrate', 'perceptive', 'performance', 'pharmacy', 'phonetic', 'photograph',
-        'pioneer', 'pocketful', 'politeness', 'positive', 'potato', 'processor',
-        'provincial', 'proximate', 'puberty', 'publisher', 'pyramid', 'quantity',
-        'racketeer', 'rebellion', 'recipe', 'recover', 'repellent',
-        'replica', 'reproduce', 'resistor', 'responsive', 'retraction', 'retrieval',
-        'retrospect', 'revenue', 'revival', 'revolver', 'sandalwood', 'sardonic',
-        'Saturday', 'savagery', 'scavenger', 'sensation', 'sociable', 'souvenir',
-        'specialist', 'speculate', 'stethoscope', 'stupendous', 'supportive', 'surrender',
-        'suspicious', 'sympathy', 'tambourine', 'telephone', 'therapist', 'tobacco',
-        'tolerance', 'tomorrow', 'torpedo', 'tradition', 'travesty', 'trombonist',
-        'truncated', 'typewriter', 'ultimate', 'undaunted', 'underfoot', 'unicorn',
-        'unify', 'universe', 'unravel', 'upcoming', 'vacancy', 'vagabond',
-        'vertigo', 'Virginia', 'visitor', 'vocalist', 'voyager', 'warranty',
-        'Waterloo', 'whimsical', 'Wichita', 'Wilmington', 'Wyoming', 'yesteryear',
-        'Yucatan'
+
+    var defaultWordList = [
+    'ack', 'alabama', 'alanine', 'alaska', 'alpha', 'angel', 'apart', 'april',
+    'arizona', 'arkansas', 'artist', 'asparagus', 'aspen', 'august', 'autumn',
+    'avocado', 'bacon', 'bakerloo', 'batman', 'beer', 'berlin', 'beryllium',
+    'black', 'blossom', 'blue', 'bluebird', 'bravo', 'bulldog', 'burger',
+    'butter', 'california', 'carbon', 'cardinal', 'carolina', 'carpet', 'cat',
+    'ceiling', 'charlie', 'chicken', 'coffee', 'cola', 'cold', 'colorado',
+    'comet', 'connecticut', 'crazy', 'cup', 'dakota', 'december', 'delaware',
+    'delta', 'diet', 'don', 'double', 'early', 'earth', 'east', 'echo',
+    'edward', 'eight', 'eighteen', 'eleven', 'emma', 'enemy', 'equal',
+    'failed', 'fanta', 'fifteen', 'fillet', 'finch', 'fish', 'five', 'fix',
+    'floor', 'florida', 'football', 'four', 'fourteen', 'foxtrot', 'freddie',
+    'friend', 'fruit', 'gee', 'georgia', 'glucose', 'golf', 'green', 'grey',
+    'hamper', 'happy', 'harry', 'hawaii', 'helium', 'high', 'hot', 'hotel',
+    'hydrogen', 'idaho', 'illinois', 'india', 'indigo', 'ink', 'iowa',
+    'island', 'item', 'jersey', 'jig', 'johnny', 'juliet', 'july', 'jupiter',
+    'kansas', 'kentucky', 'kilo', 'king', 'kitten', 'lactose', 'lake', 'lamp',
+    'lemon', 'leopard', 'lima', 'lion', 'lithium', 'london', 'louisiana',
+    'low', 'magazine', 'magnesium', 'maine', 'mango', 'march', 'mars',
+    'maryland', 'massachusetts', 'may', 'mexico', 'michigan', 'mike',
+    'minnesota', 'mirror', 'mississippi', 'missouri', 'mobile', 'mockingbird',
+    'monkey', 'montana', 'moon', 'mountain', 'muppet', 'music', 'nebraska',
+    'neptune', 'network', 'nevada', 'nine', 'nineteen', 'nitrogen', 'north',
+    'november', 'nuts', 'october', 'ohio', 'oklahoma', 'one', 'orange',
+    'oranges', 'oregon', 'oscar', 'oven', 'oxygen', 'papa', 'paris', 'pasta',
+    'pennsylvania', 'pip', 'pizza', 'pluto', 'potato', 'princess', 'purple',
+    'quebec', 'queen', 'quiet', 'red', 'river', 'robert', 'robin', 'romeo',
+    'rugby', 'sad', 'salami', 'saturn', 'september', 'seven', 'seventeen',
+    'shade', 'sierra', 'single', 'sink', 'six', 'sixteen', 'skylark', 'snake',
+    'social', 'sodium', 'solar', 'south', 'spaghetti', 'speaker', 'spring',
+    'stairway', 'steak', 'stream', 'summer', 'sweet', 'table', 'tango', 'ten',
+    'tennessee', 'tennis', 'texas', 'thirteen', 'three', 'timing', 'triple',
+    'twelve', 'twenty', 'two', 'uncle', 'undress', 'uniform', 'uranus', 'utah',
+    'vegan', 'venus', 'vermont', 'victor', 'video', 'violet', 'virginia',
+    'washington', 'west', 'whiskey', 'white', 'william', 'winner', 'winter',
+    'wisconsin', 'wolfram', 'wyoming', 'xray', 'yankee', 'yellow', 'zebra',
+    'zulu'
     ];
 
-    HumanHash = {};
+    HumanHasher = function(wordList){
+        this.wordlist = (wordList && (wordList.length === 256)) ? wordList : defaultWordList;
+        return this;
+    };
 
-    HumanHash.humanize = function(digest, numWords, separator){
-        var chars, i, len, hex, output=[];
+    HumanHasher.prototype.humanize = function(digest, numWords, separator){
+        var chars, i, len, hex, output=[], bytes=[], compressedBytes=[];
 
         // defaults
         numWords = numWords || 4;
@@ -113,12 +61,53 @@
         // a single hex value
         for(i = 0, len = chars.length; i < len && output.length < numWords; i+=2){
             hex = parseInt(chars[i]+(chars[i+1] || '0'),16);
-            // use even or odd list depending on current position in array
-            output.push((i/2)%2==0 ? evenWords[hex] : oddWords[hex]);
+            bytes.push(hex);
         }
+
+        compressedBytes = this.compress(bytes, numWords);
+
+        for( var i = 0; i < numWords; i += 1 ){
+            output.push( this.wordlist[compressedBytes[i]] );
+        };
 
         return output.join(separator);
     };
 
-    global.HumanHash = HumanHash;
+    HumanHasher.prototype.compress = function(bytes, target){
+        // Compress a list of byte values to a fixed target length.
+
+        // A smaller number of bytes will be zero-padded to a larger number as needed
+        for( var i = target - bytes.length; i > 0; i -= 1 ){
+            bytes.push(0);
+        }
+
+        if( target === bytes.length ){
+            return bytes;
+        }
+
+        var bytesLength = bytes.length,
+            seg_size = Math.floor(bytesLength / target),
+            segments = [],
+            seg_num = 0;
+
+        // Split `bytes` into `target` segments.
+        // XOR is used for compression.
+        // Left-over bytes are caught in the last segment.
+        for( var i = 0, ii = bytesLength; i < ii ; i += 1 ){
+            seg_num = Math.min(Math.floor(i/seg_size),target-1);
+            segments[seg_num]  = (segments[seg_num] !== undefined ? segments[seg_num] ^ bytes[i] : bytes[i]);
+        }
+
+        return segments;
+    };
+
+    HumanHasher.prototype.uuid = function(numWords, separator){
+        digest = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c){
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+        return [this.humanize(digest, numWords, separator), digest];
+    };
+
+    global.HumanHasher = HumanHasher;
 })(typeof module == 'object' ? module.exports : (window || global));
